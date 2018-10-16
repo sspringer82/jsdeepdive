efa9f2e08eafda0f33879d1dafc917e40cbac1ff
Sebastian Springer <info@sebastian-springer.com>
2018-10-16 13:47:09 +0200

    node_modules

--- server/index.js
+++ server/index.js
@@ -3,3 +3,9 @@ fetch('http://localhost:3000/users')
     return response.json();
   })
   .then(data => console.log(data));
+
+function createNode(data) {
+  const div = document.createElement('div');
+  div.innerText = data.name;
+  document.querySelector('body').append(div);
+}
